7d5bbf3390a903f899ca37014137a331b6059800
Mason Burke <masonburke@rocketmail.com>
2020-06-01 14:59:31 -0600

    if needed, pull these for the auth0 api, but currently throws error

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
+import PrivateRoute from "./components/PrivateRoute";
+import React from "react";
+import NavBar from "./components/NavBar";
+import ExternalApi from "./views/ExternalApi";
+import { useAuth0 } from "./react-auth0-spa";
+
+
+
 
 function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Don't forget to include the history module */}
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          {/* <Route path="/profile" component={Profile} /> */}
+        <PrivateRoute path="/profile" component={Profile} />
+        <PrivateRoute path="/external-api" component={ExternalApi} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,24 +1,41 @@
 const express = require("express");
+const cors = require("cors");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
 
-const mongoose = require("mongoose");
-const routes = require("./routes");
+// Create a new Express app
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
+// Accept cross-origin requests from the frontend app
+app.use(cors({ origin: 'http://localhost:3000' }));
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// Set up Auth0 configuration
+const authConfig = {
+  domain: "YOUR_DOMAIN",
+  audience: "YOUR_API_IDENTIFIER"
+};
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// Define middleware that validates incoming bearer tokens
+// using JWKS from YOUR_DOMAIN
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ["RS256"]
 });
+
+// Define an endpoint that must be called with an access token
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your Access Token was successfully validated!"
+  });
+});
+
+// Start the app
+app.listen(3001, () => console.log('API listening on 3001'));
