0851ff8419fa8249746be2854a3a56a15b6e3b0c
Tao <tao8261@aol.com>
2020-06-03 10:01:30 -0600

    work on cheapsearch api

--- client/src/components/CheapSearch.js
+++ client/src/components/CheapSearch.js
@@ -1,10 +1,14 @@
-import React from "react"
+import {useState, useEffect} from "react"
 import { Button, Form } from 'semantic-ui-react'
 // import FlightResults from "/FlightResults"
 
 
 const CheapSearch = () => {
-      
+
+      const [depCity, setDepCity] = useState("");
+      const [arrCity, setArrCity] = useState("");
+      const [price, setPrice] = useState("")
+
 
 
       return (
