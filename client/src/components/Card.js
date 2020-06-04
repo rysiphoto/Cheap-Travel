77b8c52effeffe90078ed1ccf84d682e67ca1337
Mason Burke <masonburke@rocketmail.com>
2020-06-04 12:56:39 -0600

    posting works to travel journal

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -6,45 +6,36 @@ import {
    } from "semantic-ui-react"
 import { Link } from "react-router-dom"
 import { ListItem, List } from "./List";
-import JournalForm from "./Form"
-
-const Card = () => {
-  
-  const[userData, setUserData] = useState({
-      name: "",
-      date: Date.now(),
-      body: "",
-      depCity:"",
-      arrCity:"",
-      price:Number,
-  })
-  const {name, date, body, depCity, arrCity, price} = userData;
-
-const handleSubmit = e => {
-  e.preventDefault()
-}
 
 
+const Card = ({post}) => {
+
+  const {author, date, body, depCity, arrCity, price} = post;
 
     return (
      
         <Container>
         <Item.Group divided>
           <Item>
-            <Item.Image src='/images/wireframe/image.png' />
+            {/* <Item.Image src='/images/wireframe/image.png' /> */}
             <List>
-              <Item.Header name="name" as='a'>'s Travel Experience</Item.Header>
-              <ListItem>
-                <span></span>
-                <span></span>
-              </ListItem>
+              <Item.Header name="author" as='a' >{author}'s Travel Experience</Item.Header>
+
               <Item.Description>
-                
+               Flying from <b> {depCity} </b>
               </Item.Description>
+              <Item.Description>
+                Flying to <b>{arrCity}</b>
+              </Item.Description><br/>
+              <Item.Description>
+            {body}
+              </Item.Description>
+{/*               
+         
               <Item.Extra>
                 <Image avatar circular src='/images/wireframe/square-image.png' />
                
-              </Item.Extra>
+              </Item.Extra> */}
             </List>
           </Item>
           </Item.Group>
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -4,66 +4,37 @@ import Card from "./Card"
 import API from "../utils/Api"
 
 
-const JournalForm = (props) => {
-   const[userData, setUserData] = useState({
-    name: "",
-    date: Date.now(),
-    body: "",
-    depCity: "",
-    arrCity: "",
-    price: Number,
+const JournalForm = ({ author, body, price, depCity, arrCity, date, handleChange, handleSubmit}) => {
 
-})
-// const [name, setName] = useState()
-// const {name, date, body, depCity, arrCity, price} = userData;
+  return (
+   <> 
+    <Form >
+      <Form.Group widths='equal'>
+        <Form.Input 
+         fluid label='Name' placeholder='Name' defaultValue={author} name="author"
+         onChange={handleChange}/>
+        <Form.Input fluid label='Departing City' name="depCity" defaultValue={depCity} placeholder='Departing City'
+        onChange={handleChange} />
+       <Form.Input fluid label='Arriving City' name="arrCity" defaultValue={arrCity} placeholder='Arriving City'
+       onChange={handleChange} />
+       <Form.Input type="number" name="price" defaultValue={price} fluid label='Flight Price' placeholder='Flight Price USD' 
+       onChange={handleChange}/>
+      </Form.Group>
+      <Form.TextArea label='About' name="body" defaultValue={body} placeholder='Tell us more about your trip'
+      onChange={handleChange} />
+      <Form.Button onClick={handleSubmit} >Submit</Form.Button>
+    </Form>
+    
+    <div>
 
-const handleSubmit = e => {
-// e.preventDefault()
-// API.post({
-//   author:
-// })
 
+</div>
+    
+
+    </>
+  )
 }
 
-  const handleChange = (e) => {
-    const {name, value} = e.target
-    setUserData(previousState => ({...previousState, [name]:value})
-  )}
- 
-  
-
-  // {e=> setUserData(...userData, {name:e.target.value})}
-    // const { value } = this.state
-    return (
-     <> 
-      <Form >
-        <Form.Group widths='equal'>
-          <Form.Input 
-           fluid label='Name' placeholder='Name' defaultValue={userData.name} name="name"
-           onChange={handleChange}/>
-          <Form.Input fluid label='Departing City' name="depCity" defaultValue={userData.depCity} placeholder='Departing City'
-          onChange={handleChange} />
-         <Form.Input fluid label='Arriving City' name="arrCity" defaultValue={userData.arrCity} placeholder='Arriving City'
-         onChange={handleChange} />
-         <Form.Input type="number" name="price" defaultValue={userData.price} fluid label='Flight Price' placeholder='Flight Price USD' 
-         onChange={handleChange}/>
-        </Form.Group>
-        <Form.TextArea label='About' name="body" defaultValue={userData.body} placeholder='Tell us more about your trip'
-        onChange={handleChange} />
-        <Form.Button onClick={handleSubmit} >Submit</Form.Button>
-      </Form>
-      
-      <div>
-
-<Card
-name
-/>
-  </div>
-      
-
-      </>
-    )
-  }
 
 
 export default JournalForm 
