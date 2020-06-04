5c4e075fda0cf5852e0be819ff9d38fc3aba88cc
Tao <tao8261@aol.com>
2020-06-03 18:08:05 -0600

    cheapsearch functionality improved, able to pass corresponding airport code when city is selected

--- client/src/components/CheapSearch.js
+++ client/src/components/CheapSearch.js
@@ -1,9 +1,20 @@
-import React, { useState, useEffect } from "react"
-import { Button, Form } from 'semantic-ui-react'
+import React, { useState } from "react"
+import { Button, Form, Select} from 'semantic-ui-react'
 import API from "../utils/Api"
 // import FlightResults from "/FlightResults"
 
 const CheapSearch = () => {
+
+      // const state = { 
+                 
+      //             price: Number,
+      //             depCity: "",
+      //             arrCity: "",
+      //             date: ""
+      //         }
+      
+
+      
 
       const [depCity, setDepCity] = useState("");
       const [arrCity, setArrCity] = useState("");
@@ -20,11 +31,44 @@ const CheapSearch = () => {
 
 
 
-      const handleInputChange = event => {
+      const handleInputChange = (event, data) => {
             setDepCity(event.target.value)
             setArrCity(event.target.value)
             setDate(event.target.value)
+            console.log("Corresponding Airport Code Should Print Here:", data.value)
+            
       }
+
+      const cityOptions = [
+            { key: '1', text: 'Atlanta, GA', value: 'ATL-sky' },
+            { key: '2', text: 'Los Angeles, CA', value: 'LAX-sky' },
+            { key: '3', text: 'Chicago, IL', value: 'ORD-sky' },
+            { key: '4', text: 'Dallas, TX', value: 'DFW-sky' },
+            { key: '5', text: 'Denver, CO', value: 'DIA-sky' },
+            { key: '6', text: 'New York City, NY', value: 'JFK-sky' },
+            { key: '7', text: 'San Francisco, CA', value: 'SFO-sky' },
+            { key: '8', text: 'Seattle, WA', value: 'SEA-sky' },
+            { key: '9', text: 'Las Vegas, NV', value: 'LAS-sky' },
+            { key: '10', text: 'Orlando, FL', value: 'MCO-sky' },
+            { key: '11', text: 'Newark, NJ', value: 'EWR-sky' },
+            { key: '12', text: 'Charlotte, NC', value: 'CLT-sky' },
+            { key: '13', text: 'PHOENIX, AZ', value: 'PHX-sky' },
+            { key: '14', text: 'Houston, TX', value: 'IAH-sky' },
+            { key: '15', text: 'Miami, FL', value: 'MIA-sky' },
+            { key: '16', text: 'Boston, MA', value: 'BOS-sky' },
+            { key: '17', text: 'Minneapolis, MN', value: 'MSP-sky' },
+            { key: '18', text: 'Fort Lauderdale, FL', value: 'FLL-sky' },
+            { key: '19', text: 'Detroit, MI', value: 'DTW-sky' },
+            { key: '20', text: 'Philadelphia, PA', value: 'PHL-sky' },
+            { key: '21', text: 'Baltimore, MD', value: 'BWI-sky' },
+            { key: '22', text: 'Salt Lake City, UT', value: 'SLC-sky' },
+            { key: '23', text: 'San Diego, CA', value: 'SAN-sky' },
+            { key: '24', text: 'Washington D.C', value: 'IAD-sky' },
+            { key: '25', text: 'Tampa, Florida', value: 'TPA-sky' },
+            { key: '26', text: 'Portland, OR', value: 'PDX-sky' },
+            { key: '27', text: 'Honolulu, HI', value: 'HNL-sky' },
+           
+          ]
 
 
       // }
@@ -42,10 +86,13 @@ const CheapSearch = () => {
 
                               </ol>
                         </h4>
+                        <Select placeholder='Select your country' options={cityOptions} />
                         <Form>
                               <Form.Group unstackable widths={1}>
-                                    <Form.Input name="depCity" width={5} label='Departing City' placeholder='City You Will Be Leaving From' onChange={handleInputChange} />
-                                    <Form.Input name="arrCity" width={5} label='Destination City' placeholder='City You Will Be Flying To' />
+                                    <Form.Select className="depCity" options={cityOptions}  name="depCity" width={5} label='Departing City' placeholder='City You Will Be Leaving From' onChange={handleInputChange} />
+
+
+                                    <Form.Select options={cityOptions} name="arrCity" width={5} label='Destination City' placeholder='City You Will Be Flying To' />
                               </Form.Group>
                               <Form.Group widths={2}>
                                     <Form.Input name="date" width={5} type="date" label='Departure Date' />
@@ -59,6 +106,9 @@ const CheapSearch = () => {
             </>
       )
 }
+
+
+
 // state = { 
 //       author:"",
 //       body:"",
